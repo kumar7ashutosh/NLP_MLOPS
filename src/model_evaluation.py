0d94346dc171143e5bbbd1b397e29f13bc2b84d7
kumar7ashutosh <kumarashutoshbtech2023@gmail.com>
2025-09-21 22:39:51 +0530

    dvc

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -42,7 +42,22 @@ def load_model(file_path:str):
     except Exception as e:
         logger.error('Unexpected error occurred while loading the model: %s', e)
         raise
-    
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 def load_data(file_path:str)->pd.DataFrame:
     try:
         df=pd.read_csv(file_path,encoding='latin')
@@ -79,12 +94,18 @@ def evaluate_model(clf,x_test:np.ndarray,y_test:np.ndarray)->dict:
         raise
 def main():
     try:
+        params = load_params(params_path='params.yaml')
         clf=load_model('./models/model.pkl')
         test_data=load_data('./data/processed/test_tfidf.csv')
         x_test=test_data.iloc[:,:-1].values
         y_test=test_data.iloc[:,-1].values
         metrics=evaluate_model(clf,x_test=x_test,y_test=y_test)
-        
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy', accuracy_score(y_test, y_test))
+            live.log_metric('precision', precision_score(y_test, y_test))
+            live.log_metric('recall', recall_score(y_test, y_test))
+
+            live.log_params(params)
         eval_path=os.path.join('reports','metrics.json')
         os.makedirs(os.path.dirname(eval_path),exist_ok=True)
         with open(eval_path,'w') as file:
